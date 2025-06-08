07d57359af5958ab3f5d7d81caf73fb11f64b37e
Canh Ta <56242791+canhta@users.noreply.github.com>
2025-06-08 22:12:51 +0700

    chore: add husky and lint-staged

---
+++ .husky/_/husky.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# husky shell
+if [ -z "$husky_skip_init" ]; then
+  debug() {
+    [ -n "$HUSKY_DEBUG" ] && echo "husky: $*"
+  }
+  readonly hook_name="$(basename "$0")"
+  debug "starting $hook_name..."
+  if [ "$HUSKY" = "0" ]; then
+    debug "HUSKY env variable is set to 0, skipping hook" && exit 0
+  fi
+  if [ -f ~/.huskyrc ]; then
+    debug "sourcing ~/.huskyrc"
+    . ~/.huskyrc
+  fi
+  export readonly husky_skip_init=1
+  sh -e "$0" "$@"
+  exit $?
+fi
